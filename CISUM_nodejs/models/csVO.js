0f8f74310e8e6b6c5faefd7b8a08b18e0d2984c4
sun5066 <alstjr4434@naver.com>
2020-10-21 02:31:49 +0900

    10-21

--- CISUM_nodejs/models/csVO.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require("mongoose");
-var schema = mongoose.Schema;
-
-var csVO = new schema({
-    cs_id: String,
-    cs_title: String
-});
-
-module.exports = mongoose.model("tbl_cs", csVO);
