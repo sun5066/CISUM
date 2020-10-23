df5ca80cfa904ad0e8459274b6317b85f1564123
sun5066 <alstjr4434@naver.com>
2020-10-23 16:34:42 +0900

    10-23

--- CISUM/public/javascripts/first-connect.js
+++ CISUM/public/javascripts/first-connect.js
@@ -2,4 +2,4 @@ document.addEventListener("DOMContentLoaded", function () {
     setTimeout(function () {
         document.getElementById("first-connect-body").style.display = "none";
     }, 1400);
-})
+});
